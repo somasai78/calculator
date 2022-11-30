75baac209cd231c9211c7d3dc93d90c5c1265a7c
somasai78 <118876352+somasai78@users.noreply.github.com>
2022-11-30 23:07:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let outputscreen=document.getElementById("output-screen");
+function display(num){
+    outputscreen.value+=num;
+}
+function Calculate(){
+    try{
+        outputscreen.value=eval(outputscreen.value);
+    }
+    catch(err){
+        alert("Invalid")
+    }
+}
+function Clear(){
+    outputscreen.value="";
+}
+function del(){
+    outputscreen.value=outputscreen.value.slice(0,-1);
+}
